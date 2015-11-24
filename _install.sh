b95ce164571ad4e7ca394972d623030c943c8829
Roberto <io_robertob@hotmail.com>
2015-11-23 23:20:35 -0800

    Create Installer

--- _install.sh
+++ _install.sh
@@ -1,19 +1,28 @@
+
+# Install Mac's Command Line Developer Tools 
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" -v;
 # Go to Nupic website
 # https://github.com/numenta/nupic
 
 # Follos instructions, install nupic by typing in the teminal
 # anywhere, i.e. /Users/USERNAME/
 # --user flat to install in a non sytem location  
-pip install nupic --user
+#pip install nupic --user
 
 #
- if # pip is not installed
+ #if pip is not installed
 # python --version 
 # to check what python we have
 sudo easy_install pip
 
 # again type
-pip install nupic --user
+#pip install nupic --user
 
 # ERROR MESSAGE might show asking to Install Command Line Developer Tools
 # Press Install - 5 min.
@@ -29,13 +38,6 @@ git clone https://github.com/numenta/nupic.git
 sudo pip install pytest 
 
 
-# Clone the projec repository
-git clone https://github.com/iorobertob/audioNupicDMX.git
-
-# Go to project folder
-cd audioNupicDMX
-
-
 #Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
@@ -49,5 +51,12 @@ pip install PortAudio --user
 #Install PySerial
 pip install pySerial --user
 
+
+# Clone the projec repository
+git clone https://github.com/iorobertob/audioNupicDMX.git
+
+# Go to project folder
+cd audioNupicDMX
+
 # RUN
-python AND_6.py
+#python AND_6.py
